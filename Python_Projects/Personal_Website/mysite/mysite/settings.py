6dc4f393e6db99b059b71af6589fcb8a7b71ee85
DoubleA176 <alecaustinimportant@gmail.com>
2023-03-01 19:38:22 -0600

    [django-website-personal] AWS Lambda Setup
    Setup AWS Lambda function with Zappy

--- Python_Projects/Personal_Website/mysite/mysite/settings.py
+++ Python_Projects/Personal_Website/mysite/mysite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get("WEB_HOST"), '127.0.0.1']
 
 
 # Application definition
